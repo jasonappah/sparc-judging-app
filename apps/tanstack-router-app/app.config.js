2b4933e29c5fa004e700f324a38becddaeff1e98
Jason Antwi-Appah <hey@jasonaa.me>
2024-12-21 23:23:16 +0000

    fix: revert start config

--- apps/tanstack-router-app/app.config.js
+++ apps/tanstack-router-app/app.config.js
@@ -2,20 +2,19 @@ import MillionLint from "@million/lint";
 import { defineConfig } from "@tanstack/start/config";
 
 export default defineConfig({
-	// server: {
-	// 	preset: "vercel",
-	// },
-	// defaultSsr: false,
-	// vite: {
-	// 	plugins: [
-	// 		MillionLint.vite({
-	// 			enabled: false,
-	// 			rsc: true,
-	// 			optimizeDOM: true,
-	// 			experimental: {
-	// 				stabilize: true,
-	// 			},
-	// 		}),
-	// 	],
-	// },
+	server: {
+		preset: "vercel",
+	},
+	defaultSsr: false,
+	vite: {
+		plugins: [
+			MillionLint.vite({
+				rsc: true,
+				optimizeDOM: true,
+				experimental: {
+					stabilize: true,
+				},
+			}),
+		],
+	},
 });
